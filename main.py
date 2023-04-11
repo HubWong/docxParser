808b9806d353a8b56424a02c7732dd31837ba1ff
hub wong <wyb6688@hotmail.com>
2023-04-11 14:38:09 +0800

    add test.docx

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import docxParser
 
 if __name__=='__main__':
-    path ='D:\\cv.docx'
+    path ='J:\\Project Python\\docxParser\\assets\\test.docx'
+    print('*'*140)
     d = docxParser.DocxParser().get_content(path)
     print(d)
     pass
